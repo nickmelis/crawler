c6fcc1cdeb919f4dab62a436d7d7ee7f16f5d6b7
Nick <nick.melis@gospel.tech>
2020-09-11 21:20:15 +0100

    Adding tests

---
+++ sedna-crawler/src/test/java/com/sedna/crawler/service/CrawlerServiceTest.java
@@ -0,0 +1,122 @@
+package com.sedna.crawler.service;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.Logger;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.sedna.crawler.exception.PageProcessingException;
+import com.sedna.crawler.model.PageResults;
+import com.sedna.crawler.model.StaticAssets;
+
+@RunWith (MockitoJUnitRunner.class)
+public class CrawlerServiceTest {
+
+  @Mock
+  private WebPageProcessorService webPageProcessorService;
+  @Mock
+  private Logger logger;
+
+  @InjectMocks
+  private CrawlerService service;
+
+  @Before
+  public void setUp() {
+    // Set variable for tests - as this is set by Spring automagically in the app, we need to manually set its value in unit tests
+    ReflectionTestUtils.setField(service, "maxPagesToSearch", 100);
+  }
+
+  @Test
+  public void whenFetchingUrl_willReturnASinglePageWithNoLinks() throws PageProcessingException, MalformedURLException {
+    URL urlToVisit = new URL("https://sedna.com");
+    when(webPageProcessorService.process(urlToVisit))
+      .thenReturn(PageResults.builder()
+        .withPageUrl(urlToVisit)
+        .withStaticAssets(StaticAssets.builder().build())
+        .build());
+
+    List<PageResults> result = service.search(urlToVisit.toExternalForm());
+
+    assertThat(result, hasSize(1));
+    assertThat(result.get(0).getPageUrl(), is(urlToVisit));
+    assertThat(result.get(0).getStaticAssets().getImages(), hasSize(0));
+    assertThat(result.get(0).getStaticAssets().getCss(), hasSize(0));
+    assertThat(result.get(0).getStaticAssets().getJs(), hasSize(0));
+  }
+
+  @Test
+  public void whenFetchingUrl_willReturnTwoInterLinkedPages() throws PageProcessingException, MalformedURLException {
+    URL homePage = new URL("https://sedna.com/");
+    URL contactPage = new URL(homePage + "contact");
+    when(webPageProcessorService.process(homePage))
+      .thenReturn(PageResults.builder()
+        .withPageUrl(homePage)
+        .withLink(contactPage)
+        .withStaticAssets(StaticAssets.builder().build())
+        .build());
+    when(webPageProcessorService.process(contactPage))
+      .thenReturn(PageResults.builder()
+        .withPageUrl(contactPage)
+        .withLink(homePage)
+        .withStaticAssets(StaticAssets.builder().build())
+        .build());
+
+    List<PageResults> result = service.search(homePage.toExternalForm());
+
+    assertThat(result, hasSize(2));
+    // Home page
+    assertThat(result.get(0).getPageUrl(), is(homePage));
+    assertThat(result.get(0).getLinks(), hasSize(1));
+    assertThat(result.get(0).getLinks(), hasItem(contactPage));
+    // Contact page
+    assertThat(result.get(1).getPageUrl(), is(contactPage));
+    assertThat(result.get(1).getLinks(), hasSize(1));
+    assertThat(result.get(1).getLinks(), hasItem(homePage));
+  }
+
+  @Test
+  public void whenFetchingUrl_willReturnASinglePageWithStaticAssets() throws PageProcessingException, MalformedURLException {
+    URL urlToVisit = new URL("https://sedna.com/");
+    URL imageUrl = new URL(urlToVisit + "assets/images/image.jpg");
+    URL cssUrl = new URL(urlToVisit + "assets/css/style.css");
+    URL jsUrl = new URL(urlToVisit + "assets/js/script.js");
+
+    when(webPageProcessorService.process(urlToVisit))
+      .thenReturn(PageResults.builder()
+        .withPageUrl(urlToVisit)
+        .withStaticAssets(StaticAssets.builder()
+          .withImages(Set.of(imageUrl))
+          .withCss(Set.of(cssUrl))
+          .withJs(Set.of(jsUrl))
+          .build())
+        .build());
+
+    List<PageResults> result = service.search(urlToVisit.toExternalForm());
+
+    assertThat(result, hasSize(1));
+    assertThat(result.get(0).getPageUrl(), is(urlToVisit));
+    assertThat(result.get(0).getStaticAssets().getImages(), hasSize(1));
+    assertThat(result.get(0).getStaticAssets().getImages(), hasItem(imageUrl));
+
+    assertThat(result.get(0).getStaticAssets().getCss(), hasSize(1));
+    assertThat(result.get(0).getStaticAssets().getCss(), hasItem(cssUrl));
+
+    assertThat(result.get(0).getStaticAssets().getJs(), hasSize(1));
+    assertThat(result.get(0).getStaticAssets().getJs(), hasItem(jsUrl));
+  }
+}
---
+++ sedna-crawler/src/test/java/com/sedna/crawler/service/WebPageProcessorServiceTest.java
@@ -0,0 +1,61 @@
+package com.sedna.crawler.service;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.jsoup.Jsoup;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.Logger;
+
+import com.sedna.crawler.exception.PageProcessingException;
+import com.sedna.crawler.model.PageResults;
+
+@RunWith (MockitoJUnitRunner.class)
+public class WebPageProcessorServiceTest {
+
+  @Mock
+  private HttpService httpService;
+  @Mock
+  private Logger logger;
+
+  @InjectMocks
+  public WebPageProcessorService service;
+
+  @Test
+  public void whenProcessingProductPage_willReturnResultWithLinksAndStaticAssets() throws IOException, PageProcessingException {
+    URL productUrl = new URL("https://sedna.com/product");
+    String productHtml = "<html>"
+      + "  <head>"
+      + "    <script src=\"/static/js/abc.js\" type=\"text/javascript\" ></script>"
+      + "    <link rel=\"stylesheet\" type=\"text/css\" href=\"/static/css/style.css\">"
+      + "  </head>"
+      + "  <body>"
+      + "    <a href=\"/cart\">Blah</a>"
+      + "    <img src=\"/static/images/image.png\" alt=\"SEDNA\">"
+      + "  </body>"
+      + "</html>";
+    when(httpService.get(productUrl)).thenReturn(Jsoup.parse(productHtml, productUrl.toExternalForm()));
+
+    URL cartUrl = new URL("https://sedna.com/cart");
+    URL imageUrl = new URL("https://sedna.com/static/images/image.png");
+    URL cssUrl = new URL("https://sedna.com/static/css/style.css");
+    URL jsUrl = new URL("https://sedna.com/static/js/abc.js");
+
+    PageResults results = service.process(productUrl);
+
+    assertThat(results.getPageUrl(), is(productUrl));
+    assertThat(results.getLinks(), hasItems(cartUrl));
+    assertThat(results.getStaticAssets().getImages(), hasItems(imageUrl));
+    assertThat(results.getStaticAssets().getCss(), hasItems(cssUrl));
+    assertThat(results.getStaticAssets().getJs(), hasItems(jsUrl));
+  }
+}
