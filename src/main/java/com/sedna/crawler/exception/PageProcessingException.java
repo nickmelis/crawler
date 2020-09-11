88e8142746cae795a69cc8f3e6b7087fbb0632a6
Nick <nick.melis@gospel.tech>
2020-09-11 21:48:28 +0100

    Adding custom exception

---
+++ src/main/java/com/sedna/crawler/exception/PageProcessingException.java
@@ -0,0 +1,34 @@
+package com.sedna.crawler.exception;
+
+public class PageProcessingException extends Exception {
+
+  private static final long serialVersionUID = 6872675059528970905L;
+
+  public PageProcessingException() {
+    super();
+  }
+
+  public PageProcessingException(
+      String message,
+      Throwable cause,
+      boolean enableSuppression,
+      boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+
+  public PageProcessingException(
+      String message,
+      Throwable cause) {
+    super(message, cause);
+  }
+
+  public PageProcessingException(
+      String message) {
+    super(message);
+  }
+
+  public PageProcessingException(
+      Throwable cause) {
+    super(cause);
+  }
+}
